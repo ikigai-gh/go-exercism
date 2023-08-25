ea445efc9ba1e2c27effdf893545f22f9c7c0704
Vladislav Slepov <ikigai@ikigai.fail>
2023-08-25 16:16:56 +0300

    Dealing with regular expressions

---
+++ parsing_log_files.go
@@ -0,0 +1,43 @@
+package parsinglogfiles
+
+import "regexp"
+
+func IsValidLine(text string) bool {
+	re := regexp.MustCompile(`^\[(TRC|DBG|INF|WRN|ERR|FTL)\]`)
+	return re.MatchString(text)
+}
+
+func SplitLogLine(text string) []string {
+	re := regexp.MustCompile(`<[~*=-]*>`)
+	return re.Split(text, -1)
+}
+
+func CountQuotedPasswords(lines []string) int {
+	count := 0
+	re := regexp.MustCompile(`(?i:".*password.*")`)
+	for _, line := range lines {
+		if re.MatchString(line) {
+			count++
+		}
+	}
+
+	return count
+}
+
+func RemoveEndOfLineText(text string) string {
+	re := regexp.MustCompile(`end\-of\-line\d*`)
+	return re.ReplaceAllString(text, "")
+}
+
+func TagWithUserName(lines []string) []string {
+	var newLines []string
+	re := regexp.MustCompile(`(.*User\s+([\w\d]+))`)
+
+	for _, line := range lines {
+		// ?
+		line = re.ReplaceAllString(line, "[USR] $2 $1")
+		newLines = append(newLines, line)
+	}
+
+	return newLines
+}
